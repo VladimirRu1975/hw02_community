ef36414087662121506d2d34f05db7856593e7df
Владимир Рублев <tepee2013@yandex.ru>
2022-01-31 12:15:37 +0300

    My commit correction after revyu

--- yatube/posts/admin.py
+++ yatube/posts/admin.py
@@ -1,5 +1,7 @@
 from django.contrib import admin
-from .models import Group, Post
+
+from .models import Group
+from .models import Post
 
 
 class PostAdmin(admin.ModelAdmin):
@@ -16,8 +18,6 @@ class PostAdmin(admin.ModelAdmin):
     empty_value_display = '-пусто-'
 
 
-# При регистрации модели Post источником конфигурации для неё назначаем
-# класс PostAdmin
 admin.site.register(Post, PostAdmin)
 
 
